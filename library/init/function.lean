443f5047eed6ce858a6ff5a3a91f750282adf9d4
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-07-14 17:48:54 +0700

    remove

--- library/init/function.lean
+++ library/init/function.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura, Jeremy Avigad, Haitao Zhang
 -/
 prelude
-import init.data.prod init.logic
+import init.data.prod
 /-!
 # General operations on functions
 -/
