192400b899495c350e6abb1c5c6df8b812010ec6
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-07-15 15:14:25 +0700

    Id & Id.refl

--- src/kernel/hott/hott.cpp
+++ src/kernel/hott/hott.cpp
@@ -1,13 +1,17 @@
+#include "kernel/abstract.h"
 #include "kernel/hott/hott.h"
 #include "kernel/type_checker.h"
 #include "kernel/environment.h"
 #include "kernel/abstract_type_context.h"
 #include "kernel/kernel_exception.h"
+#include <vector>
 
 namespace lean {
 
 static name * g_hott_extension = nullptr;
 static name * g_fibrant        = nullptr;
+static name * g_Id             = nullptr;
+static name * g_Id_refl        = nullptr;
 
 struct hott_env_ext : public environment_extension {
     bool m_initialized;
@@ -39,11 +43,24 @@ environment declare_hott(environment const & env) {
 
     name u_name("u");
     level u = mk_param_univ(u_name);
+    expr Sort_u = mk_sort(u);
     expr Type_u = mk_sort(mk_succ(u));
     expr Prop   = mk_Prop();
 
+    expr alpha = mk_local("α", "α", Sort_u, mk_implicit_binder_info());
+    expr a     = mk_local("a", alpha);
+
     /* constant {u} fibrant : Type u → Prop */
     new_env = add_constant(new_env, *g_fibrant, {u_name}, mk_arrow(Type_u, Prop));
+
+    /* constant {u} Id {α : Sort u} : α → α → Sort u */
+    new_env = add_constant(new_env, *g_Id, {u_name}, Pi(alpha, mk_arrow(alpha, mk_arrow(alpha, Sort_u))));
+
+    /* constant {u} Id.refl {α : Sort u} (a : α) : Id a a */
+    new_env = add_constant(
+        new_env, *g_Id_refl, {u_name},
+        Pi(alpha, Pi(a, mk_app(mk_constant(*g_Id, {u}), alpha, a, a)))
+    );
 
     hott_env_ext ext = get_extension(env);
     if (ext.m_initialized)
@@ -70,19 +87,24 @@ bool hott_normalizer_extension::is_recursor(environment const &, name const & n)
 
 bool hott_normalizer_extension::is_builtin(environment const & env, name const & n) const {
     if (!get_extension(env).m_initialized) return false;
-    return n == *g_fibrant;
+    return n == *g_fibrant || n == *g_Id || n == *g_Id_refl;
 }
 
 void initialize_hott_module() {
     g_hott_extension = new name("hott_extension");
+    g_fibrant        = new name{"fibrant"};
+    g_Id             = new name{"Id"};
+    g_Id_refl        = new name{"Id", "refl"};
     g_ext            = new hott_env_ext_reg();
-    g_fibrant        = new name("fibrant");
 }
 
 void finalize_hott_module() {
-    delete g_fibrant;
     delete g_ext;
     delete g_hott_extension;
+
+    delete g_Id_refl;
+    delete g_Id;
+    delete g_fibrant;
 }
 
 }
